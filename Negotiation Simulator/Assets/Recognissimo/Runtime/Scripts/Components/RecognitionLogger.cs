ba858dc30d248c51f7b91a4b7a40e48756e64906
kfreijo <kirafreijo@yahoo.com>
2022-06-28 02:08:36 -0400

    added animation and recognissimo

--- Negotiation Simulator/Assets/Recognissimo/Runtime/Scripts/Components/RecognitionLogger.cs
+++ Negotiation Simulator/Assets/Recognissimo/Runtime/Scripts/Components/RecognitionLogger.cs
@@ -1,20 +1,25 @@
 using UnityEngine;
 using Recognissimo.Core; // PartialResult, Result
-using TMPro;
+//using TMPro;
 
 public class RecognitionLogger : MonoBehaviour
-    {
-        public TMP_Text wordtext;
+{
+    //public TMP_Text wordtext;
+    public string totaltext;
+    public bool ended;
 
-        public void OnPartialResult(PartialResult partialResult)
-            {
-                //Debug.Log($"<color=yellow>{partialResult.partial}</color>");
-                wordtext.text = partialResult.partial;
-            }
+    public void OnPartialResult(PartialResult partialResult)
+        {
+            Debug.Log($"<color=yellow>{partialResult.partial}</color>");
+            //wordtext.text = partialResult.partial;
+            ended = false;
+        }
 
-        public void OnResult(Result result)
-            {
-                //Debug.Log($"<color=green>{result.text}</color>");
-                wordtext.text = result.text;
-            }
-    }
+    public void OnResult(Result result)
+        {
+            Debug.Log($"<color=green>{result.text}</color>");
+            //wordtext.text = result.text;
+            totaltext = result.text;
+            ended  = true;
+        }
+}
