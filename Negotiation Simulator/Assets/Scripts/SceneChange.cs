84fb75c964073476d75421e1400c52ccdf5b4347
LAPTOP-UCTV328P\rayel <rayelisha21@gmail.com>
2022-06-30 09:44:52 -0400

    Player Camera & Rendering Fixed

---
+++ Negotiation Simulator/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,12 @@
+using System.Collections;  
+using System.Collections.Generic;  
+using UnityEngine;  
+using UnityEngine.SceneManagement;  
+public class SceneChange: MonoBehaviour {  
+    public void Scene1() {  
+        SceneManager.LoadScene("Scene1");  
+    }  
+    public void Scene2() {  
+        SceneManager.LoadScene("Scene2");  
+    }   
+}   
