e1c3dc28e76881865f39205f6413a7f7d5f317b5
kfreijo <kirafreijo@yahoo.com>
2022-07-02 17:01:03 -0400

    word lists added and structure for changing them

--- Negotiation Simulator/Assets/WordCollector.cs
+++ Negotiation Simulator/Assets/WordCollector.cs
@@ -9,16 +9,43 @@ public class WordCollector : MonoBehaviour
     public GameObject RecogLogger;
     public Player playerstats;
     public HealthBar healthbar;
+    public GameObject PowerpointChanger;
     public TMP_Text wordtext;
     public TMP_Text pointtext;
     private string lastword;
-    private string[] list1 = {"great price", "fast", "small"};
-
+    
+    private bool pregame;
+    private bool recap;
+    private bool slideshow;
+    private bool obj1;
+    private bool obj2;
+    
+    private string[] greetinglist = {"hi", "hello", "hey", "nice" , "great", "meet"};
+    private string[] recaplist = {"increase profit", "profitability", "doesn't", 
+    "does not", "offer enough", "provide", "have any", "healthy"};
+    private string[] slide1list = {"slides", "slideshow", "questions"};
+    private string[] slide2list = {"many", "a lot", "several", "options", "snacks",
+    "customers", "increase availability", "labels", "diet", "dietary", "restrictions", 
+    "small", "compact", "economic", "efficient", "clear", "packaging"};
+    private string[] slide3list = {"revenue", "sales", "commission", "increase", 
+    "payment plan", "gain customers", "new customers", "profitable"};
+    private string[] slide4list = {"increase profit", "profitability", "healthy", 
+    "retain", "gain", "small", "compact", "revenue", "money", "diet"};
+    private string[] slide5list = {"thank you", "thanks", "listening", "appreciate", 
+    "time", "questions"};
+    private string[] obj1list = {"highest", "high", "up to", "500", "five hundred", 
+    "month", "guests", "customes", "satisfied"};
+    private string[] obj2list = {"feel", "felt", "found", "trial", "hospital", "popular", 
+    "trending", "boost", "increase", "sales"};
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        pregame = true;
+        recap = false;
+        slideshow = false;
+        obj1 = false;
+        obj2 = false;
     }
 
     // Update is called once per frame
@@ -29,14 +56,31 @@ public class WordCollector : MonoBehaviour
             lastword = rl.totaltext;
             text += rl.totaltext + " ";
             wordtext.text = text;
-            foreach (var word in list1) {
-		        if (lastword.Contains(word.ToString()))
-			        AdjustPoints(1);
-	        }
-            if (lastword.Contains("um"))
-                AdjustPoints(-1);
-            if (lastword.Contains("uh"))
-                AdjustPoints(-1);
+            
+            // find the place the game is currently at
+            // call the word method with the corresponding list
+            if (pregame)
+                CheckWords(greetinglist);
+            else if (recap)
+                CheckWords(recaplist);
+            else if (slideshow){
+                int slide = PowerpointChanger.GetComponent<MovePowerpoint>().currentslide;
+                if (slide == 1)
+                    CheckWords(slide1list);
+                else if (slide == 2)
+                    CheckWords(slide2list);
+                else if (slide == 3)
+                    CheckWords(slide3list);
+                else if (slide == 4)
+                    CheckWords(slide4list);
+                else
+                    CheckWords(slide5list);
+            }
+            else if (obj1)
+                CheckWords(obj1list);
+            else if (obj2)
+                CheckWords(obj2list);
+            
         } 
     }
 
@@ -46,4 +90,37 @@ public class WordCollector : MonoBehaviour
         pointtext.text = playerstats.currentHealth.ToString();
         healthbar.SetHealth(playerstats.currentHealth);
     }
+
+    void CheckWords(string[] list)
+    {
+        foreach (var word in list) {
+		    if (lastword.Contains(word.ToString()))
+			    AdjustPoints(1);
+	    }
+        if (lastword.Contains("um"))
+            AdjustPoints(-1);
+        if (lastword.Contains("uh"))
+            AdjustPoints(-1);
+    }
+
+    void StartRecap(){
+        pregame = false;
+        recap = true;
+    }
+
+    void StartSlideshow(){
+        recap = false;
+        slideshow = true;
+    }
+
+    void StartObj1(){
+        slideshow = false;
+        obj1 = true;
+    }
+
+    void StartObj2(){
+        obj1 = false;
+        obj2 = true;
+    }
+
 }
