e1231e9a88ac93ff7b48aa689ad40209e2aa0d80
LAPTOP-UCTV328P\rayel <rayelisha21@gmail.com>
2022-07-06 11:47:59 -0400

    AutoHands readded and moving speech bubble

---
+++ Negotiation Simulator/Assets/Scripts/Billboard.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public Transform camera;
+
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + camera.forward);
+    }
+}
